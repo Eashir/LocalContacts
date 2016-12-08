259e0802833e764c86573ba29002e14404434fa3
Eashir Arafat <eashirarafat@gmail.com>
2016-12-07 21:38:04 -0500

    Updated APIRequestManager with GET & POST Requests

--- LocalContacts/APIRequestManager.swift
+++ LocalContacts/APIRequestManager.swift
@@ -13,17 +13,65 @@ class APIRequestManager {
     
     private init () {}
     
-    func getData(_ endPoint: String, callback: @escaping (Data?) -> Void) {
-        guard let myURL = URL(string: endPoint) else { return }
+    
+    func getData(_ endPoint: String, completion: @escaping (Data) -> Void) {
+        guard let url = URL(string: endPoint) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Basic a2V5LTc6UlBtMERkQTExWS1OWjZoQXduTGI=", forHTTPHeaderField: "Authorization")
         
         let session = URLSession(configuration: .default)
-        session.dataTask(with: myURL) { (data: Data?, response: URLResponse?, error:Error?) in
+        session.dataTask(with: request) { (data, response, error) in
             if error != nil {
-                print("Error \(error)")
+                print("Error encountered at get request: \(error)")
             }
             guard let validData = data else { return }
-            callback(validData)
-            
-            }.resume()
+            DispatchQueue.main.async {
+                completion(validData)
+            }
+        }.resume()
     }
+    
+    func postu(name: String, email: String) {
+        var request: URLRequest = URLRequest(url: URL(string: "https://api.fieldbook.com/v1/5846eb88a785c00300a9434b/contacts")!)
+        
+        request.httpMethod = "POST"
+        
+        let headers = [
+            "content-type": "application/json",
+            "authorization": "Basic a2V5LTc6UlBtMERkQTExWS1OWjZoQXduTGI="
+          
+        ]
+        
+        let parameters = [
+            "name": "\(name)",
+            "email": "\(email)",
+            "role": "Fighter",
+            "company": "Riot Games",
+            
+            "image": "http://pre08.deviantart.net/cb66/th/pre/f/2016/282/0/a/yasuo_by_saya94-dakdrqi.png"
+        ]
+        request.allHTTPHeaderFields = headers
+        
+        do {
+            let postData: Data = try JSONSerialization.data(withJSONObject: parameters, options: [])
+            request.httpBody = postData
+        }
+        catch {
+            print("Error converting to data")
+        }
+        
+        let session: URLSession = URLSession(configuration: .default)
+        session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            if (error != nil) {
+                print(error)
+            } else {
+                let httpResponse = response as? HTTPURLResponse
+                print(httpResponse)
+            }
+        }.resume()
+    }
+    
 }
--- LocalContacts/DetailViewController 23-58-15-489.swift
+++ LocalContacts/DetailViewController 23-58-15-489.swift
@@ -22,6 +22,7 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         self.title = contact?.name
         // Do any additional setup after loading the view.
         guard let endPoint = contact?.image else { return }
@@ -45,10 +46,6 @@ class DetailViewController: UIViewController {
     }
     
     @IBAction func clickDoneToPOST(_ sender: UIBarButtonItem) {
-        postu()
-    }
-    
-    func postu() {
         
         guard let firstName = firstNameTextField.text else {
             print("No first name given")
@@ -63,46 +60,12 @@ class DetailViewController: UIViewController {
             return
         }
         let name = firstName + " " + lastName
-        
-        var request: URLRequest = URLRequest(url: URL(string: "https://api.fieldbook.com/v1/5846eb88a785c00300a9434b/contacts")!)
-        
-        request.httpMethod = "POST"
-        
-        let headers = [
-            "content-type": "application/json",
-            "authorization": "Basic a2V5LTc6UlBtMERkQTExWS1OWjZoQXduTGI=",
-            "cache-control": "no-cache",
-            "postman-token": "395019a8-b41c-69ad-bd9d-c0eb14dc3e33"
-        ]
-        
-        let parameters = [
-            "name": "\(name)",
-            "email": "\(email)",
-            "role": "Fighter",
-            "company": "Riot Games",
-            
-            "image": "http://pre08.deviantart.net/cb66/th/pre/f/2016/282/0/a/yasuo_by_saya94-dakdrqi.png"
-        ]
-        request.allHTTPHeaderFields = headers
-        
-        do {
-            let postData: Data = try JSONSerialization.data(withJSONObject: parameters, options: [])
-            request.httpBody = postData
-        }
-        catch { 
-            print("Error converting to data")
-        }
-        
-        let session: URLSession = URLSession(configuration: .default)
-        session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
-            if (error != nil) {
-                print(error)
-            } else {
-                let httpResponse = response as? HTTPURLResponse
-                print(httpResponse)
-            }
-        }.resume()
+        APIRequestManager.manager.postu(name: name, email: email)
     }
+    
+    
+    
+
     
     
     
